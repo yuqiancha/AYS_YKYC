6f0c452a1456c0017ec505ff0bc373b4285905e3
yuqiancha <ping_jw@126.com>
2018-04-27 22:41:19 +0800

    Add CalcuError
    增加比对软件

---
+++ CalcuError/CalcuError/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace CalcuError
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Key = new System.Windows.Forms.TextBox();
+            this.textBox_Value = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Error = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "输入220字节比对值";
+            // 
+            // textBox_Key
+            // 
+            this.textBox_Key.Location = new System.Drawing.Point(219, 120);
+            this.textBox_Key.Multiline = true;
+            this.textBox_Key.Name = "textBox_Key";
+            this.textBox_Key.Size = new System.Drawing.Size(489, 108);
+            this.textBox_Key.TabIndex = 1;
+            // 
+            // textBox_Value
+            // 
+            this.textBox_Value.Location = new System.Drawing.Point(219, 39);
+            this.textBox_Value.Multiline = true;
+            this.textBox_Value.Name = "textBox_Value";
+            this.textBox_Value.Size = new System.Drawing.Size(489, 56);
+            this.textBox_Value.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 56);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "载入要分析的文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(45, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 61);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "开始分析";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(526, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "误码率";
+            // 
+            // textBox_Error
+            // 
+            this.textBox_Error.Location = new System.Drawing.Point(595, 281);
+            this.textBox_Error.Name = "textBox_Error";
+            this.textBox_Error.Size = new System.Drawing.Size(100, 28);
+            this.textBox_Error.TabIndex = 7;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(169, 278);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "总数据量";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(169, 323);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 28);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 326);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "总帧数";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(404, 281);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 28);
+            this.textBox3.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(300, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 18);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "错误字节数";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(404, 326);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 28);
+            this.textBox4.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(300, 329);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 18);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "错误帧数";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 407);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_Error);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_Value);
+            this.Controls.Add(this.textBox_Key);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Key;
+        private System.Windows.Forms.TextBox textBox_Value;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Error;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ CalcuError/CalcuError/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CalcuError
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Byte[] ReadBuf;
+        long ReadNums=0;
+        int FrameNums=0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                openFileDialog1.InitialDirectory = Program.GetStartupPath();
+                try
+                {
+                    textBox_Value.Text = openFileDialog1.FileName;   
+                    ReadBuf = System.IO.File.ReadAllBytes(openFileDialog1.FileName);
+                    ReadNums = ReadBuf.Length;
+                    textBox1.Text = ReadNums.ToString();
+                    FrameNums = (int)(ReadNums / 220);
+                    textBox2.Text = FrameNums.ToString();
+                }
+                catch
+                {
+                    // MyLog.Error("加载发送码本失败！");             
+                    //MessageBox.Show("运行日志打开失败！");
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string temp = textBox_Key.Text.Replace(" ", "");
+
+            if(ReadNums<220)
+            {
+                MessageBox.Show("选择文件内容长度小于1帧，请重新选择！");
+                return;
+            }
+            else if(temp.Length != 440)
+            {
+                MessageBox.Show("输入比对内容长度小于220Byte，请重新选择！");
+                return;
+            }
+            else
+            {
+                byte[] KeyBuf = StrToHexByte(temp);
+
+                int ErrorBytes = 0;
+                int ErrorFrames = 0;
+                int pos = 0;
+
+                for(int i=0;i<FrameNums;i++)
+                {
+                    byte[] tempBuf = new byte[220];
+                    Array.Copy(ReadBuf, pos, tempBuf, 0, 220);
+                    pos = pos + 220;
+                    bool FrameRight = true;
+                    for(int j=0;j<220;j++)
+                    {
+                        if (tempBuf[j] != KeyBuf[j])
+                        {
+                            ErrorBytes += 1;
+                            FrameRight = false;
+                        }
+                    }
+                    if (!FrameRight) ErrorFrames += 1;
+                    
+                }
+
+                textBox3.Text = ErrorBytes.ToString();
+                textBox4.Text = ErrorFrames.ToString();
+
+                textBox_Error.Text = (((double)ErrorBytes / (double)ReadNums)*100).ToString("#0.0000")+"%";
+            }
+        }
+
+        private static byte[] StrToHexByte(string hexString)
+        {
+            hexString = hexString.Replace(" ", "").Replace("\r", "").Replace("\n", "");
+            if ((hexString.Length % 2) != 0)
+                hexString += " ";
+
+            byte[] returnBytes = new byte[hexString.Length / 2];
+
+            for (int i = 0; i < returnBytes.Length; i++)
+                returnBytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            return returnBytes;
+
+        }
+
+    }
+}
